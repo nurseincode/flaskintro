40c98e2f9d1b93e9414bec06d6f85bb3b71631ec
https://github.com/nurseincode <nurseincode@gmail.com>
2025-12-21 18:03:28 +1100

    added changes to app

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+products = [
+    {
+        "id": 1,
+        "title": 'Product 1',
+        "price": 150
+    },
+    {
+        "id": 2,
+        "title": 'Product 2',
+        "price": 400
+    }
+]
+# print(type(products))
+
 @app.route('/')
 def hello():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!, Goodbye!</h1>'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+@app.route('/second')
+def another_route():
+    return('<h2> This is another route!</h2>')
+
+@app.route('/products')
+def all_products():
+    return jsonify(products)
+
+@app.route('/can-vote')
+def can_vote():
+    age = 20
+    if age >= 18:
+        return {"message": 'You can Vote!', "can_vote": True}
+    else:
+        return {"message": 'Not old enough to vote!', "can_vote": False}
